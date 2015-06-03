a9ce465fefa3da23f57cb4398e416adce998781d
Alexander Simonov <alex@simonov.me>
2015-06-03 15:05:19 +0300

    More info about regexp

--- lib/rfinvoice/coercion/nmtoken.rb
+++ lib/rfinvoice/coercion/nmtoken.rb
@@ -4,7 +4,31 @@ module RFinvoice
       include ::RFinvoice::Coercion::Base
       include ::RFinvoice::Coercion::Limiting
       include ::RFinvoice::Coercion::Patterning
-      PATTERN = /\A[\-\.0-9:A-Z_a-z\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C-\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]*\z/
+      PATTERN = /\A*\z/
+      # XML NameChar pattern http://www.w3.org/TR/2000/WD-xml-2e-20000814#NT-NameChar
+      # Regexp from http://www.regular-expressions.info/shorthand.html
+      PATTERN = /
+        \A            # Start of line
+        [
+          \-\.:_          # Supported special symbols
+          0-9             # Numeric character
+          A-Za-z          # Alphabetic character
+          \u00B7          # Merged Unicode ranges. Possible what they are not includes all.
+          \u00C0-\u00D6
+          \u00D8-\u00F6
+          \u00F8-\u037D
+          \u037F-\u1FFF
+          \u200C-\u200D
+          \u203F
+          \u2040
+          \u2070-\u218F
+          \u2C00-\u2FEF
+          \u3001-\uD7FF
+          \uF900-\uFDCF
+          \uFDF0-\uFFFD
+        ]*
+        \z            # End of line
+      /x
       strict true
 
       def coerce(value)
